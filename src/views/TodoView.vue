25647d455fa284074fd4b80bd27dfa91ca53e61f
Moonzal <muntaharizal@gmail.com>
2024-01-02 23:18:41 +0700

    hampir selesai

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,23 +1,73 @@
 <script setup>
+import { Icon } from '@iconify/vue';
 import { ref } from "vue";
 import { uid}  from "uid";
 import TodoCreator from '@/components/TodoCreator.vue';
+import TodoItem from "@/components/TodoItem.vue";
 
 const todoList = ref([]);
-const createTodo =(todo) => {
+
+const fetchTodoList = () => {
+  const savedTodoList = JSON.parse(localStorage.getItem("todoList"));
+  if (savedTodoList) {
+    todoList.value = savedTodoList;
+  }
+};
+
+fetchTodoList();
+
+const setTodoListLocalStorage = () => {
+  localStorage.setItem("todoList", JSON.stringify(todoList.value));
+};
+
+const createTodo = (todo) => {
   todoList.value.push({
-    id:uid(),
+    id: uid(),
     todo,
-    isCompleted: null,
+    isCompleted: false,
     isEditing: null,
-  })
-}
+  });
+  setTodoListLocalStorage();
+};
+
+const toggleEditTodo = (todoPos) => {
+  todoList.value[todoPos].isEditing = !todoList.value[todoPos].isEditing;
+  setTodoListLocalStorage();
+};
+
+const updateTodo = (todoVal, todoPos) => {
+  todoList.value[todoPos].todo = todoVal;
+  setTodoListLocalStorage();
+};
+
+const deleteTodo = (todoId) => {
+  todoList.value = todoList.value.filter((todo)=> todo.id !== todoId);
+  setTodoListLocalStorage();
+};
+
+const toggleTodoComplete = (todoPos) =>{
+  todoList.value[todoPos].isCompleted = !todoList.value[todoPos].isCompleted;
+  setTodoListLocalStorage();
+};
 </script>
 
 <template>
   <main>
     <h1>Buat Todo</h1>
     <TodoCreator @create-todo="createTodo"></TodoCreator>
+    <ul class="todo-list" v-if="todoList.length > 0">
+        <TodoItem
+        v-for="(todo, index) in todoList"
+        :todo="todo"
+        :index="index"
+        @toggle-complete="toggleTodoComplete" 
+        @edit-todo="toggleEditTodo"
+        @update-todo="updateTodo"
+        @delete-todo="deleteTodo"/>
+    </ul>
+    <p class="todos-msg" v-else><Icon icon="noto:sad-but-relieved-face" width="22" />
+      <span>Gaada Hal yang mau dilakukan, tambahin sana!</span>
+    </p>
   </main>
 </template>
 
@@ -33,6 +83,22 @@ main {
   h1 {
     margin-bottom: 16px;
     text-align: center;
+  }
+
+  .todo-list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+  }
+
+  .todos-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
   }
 }
 </style>
